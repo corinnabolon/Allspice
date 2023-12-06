356401fd501804b626d84305565b77f6a37df934
Corinna Bolon <kyndrill@rocketmail.com>
2023-12-06 15:34:49 -0700

    added some notes

--- client/src/services/FavoritesService.js
+++ client/src/services/FavoritesService.js
@@ -7,24 +7,31 @@ import { api } from "./AxiosService.js"
 
 class FavoritesService {
 
+
+  //Note: recipeId == a recipe.id that we clicked on; we turn it into a recipeId because CreateFavorite on the back end creates a many-to-many with an accountId and a recipeId, so we have to post / create it in that format
   async createFavorite(recipeId) {
     let foundRecipe = AppState.recipes.find(recipe => recipe.id == recipeId)
+    logger.log("Foundrecipe", foundRecipe)
     foundRecipe.recipeId = foundRecipe.id
     const res = await api.post("api/favorites", foundRecipe)
     foundRecipe.accountId = res.data.accountId
     foundRecipe.favoriteId = res.data.id
+    //favoriteId is the ID of the many-to-many itself--what we will need to delete it--whereas the foundRecipe.id matches the ID of the recipe which the FavoritedRecipe we make extends from
     let newFavorite = new FavoritedRecipe(foundRecipe)
     AppState.myFavoriteRecipes.push(newFavorite)
   }
 
   async removeFavorite(recipeId) {
     let favoriteToDelete = AppState.myFavoriteRecipes.find(favorite => favorite.id == recipeId)
+    //id of the associated favorite should match the id of the recipe
     if (!favoriteToDelete) {
       return
     }
     let favoriteId = favoriteToDelete.favoriteId
+    //favoriteId is what we need to pass through to the URL to delete it (the many-to-many)
     const res = await api.delete(`api/favorites/${favoriteId}`)
     let index = AppState.myFavoriteRecipes.findIndex(favorite => favorite.id == recipeId)
+    //the favorite.id and recipe.id will match
     AppState.myFavoriteRecipes.splice(index, 1)
   }
 
